3bd358cb04cb353ac924006f277ecdc01d098bf8
Jonatan Iglesias <srmartigan@gmail.com>
2022-06-17 19:11:15 +0200

    inicio branch Ver 1.4 con LARAVEL 9

--- app/Http/Controllers/Api/HomeController.php
+++ app/Http/Controllers/Api/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
             ],401);
         }
         //fin validar Toquen-------------------------
-var_dump("esoty aqui en master");
+var_dump("estoy aqui en branch 1.4");
         $listadoPartesDiario = Helper::queryListadoPartesDiarioApi(
             Helper::getMesActual(),
             Helper::getYearActual(),
