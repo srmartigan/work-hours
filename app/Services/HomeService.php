ba6a8e22166132ff5103bb6b2996b12e46b6a851
srmartigan <srmartigan@gmail.com>
2022-09-05 20:42:55 +0200

    Implementando HomeService.php

--- app/Services/HomeService.php
+++ app/Services/HomeService.php
@@ -1,8 +1,29 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Services;
+
+use App\Domain\HomeDto;
+use App\Helper;
 
 class HomeService
 {
 
+    public function __construct()
+    {
+    }
+
+    public function execute(HomeDto $datos): array
+    {
+        $listadoPartesDiario = Helper::queryListadoPartesDiarioApi(
+            $datos->mes,
+            $datos->year,
+            $datos->id
+        );
+        $datos->totalHoras = Helper::sumarHorasNormales($listadoPartesDiario);
+        $datos->total = Helper::calcularTotalPrecioNormalApi($datos->totalHoras,$datos->id);
+
+        return $datos->datos();
+    }
 }
